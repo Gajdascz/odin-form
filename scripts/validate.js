0bb1407212786a69e4a40ab5da9d079b79b190e5
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-11 20:24:34 -0400

    Fix password error msg and add validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,14 +1,14 @@
 const passwordField = document.querySelectorAll(`input[type='password']`);
 const passwordLabel = document.querySelector(`label[for='password']`);
 const confirmPasswordLabel = document.querySelector(`label[for='confirm-password']`);
-const formInputs = document.querySelector(`.form-inputs-container`);
-const mistMatchError = document.createElement(`div`);
-mistMatchError.classList.add(`mismatch-error`);
+const passwordMatchErrorSpan = document.querySelector(`.password-match-error`);
 
-console.log( passwordField[0].value);
 passwordField.forEach((input) => {
   input.addEventListener(`keyup`, (e) => {
-    if(passwordField[0].value !== passwordField[1].value){
+    if(passwordField[0].value !== passwordField[1].value ||
+       passwordField[0].value === "" || passwordField[0] === null ||
+       passwordField[1].value === "" || passwordField[1] === null
+      ){
       passwordField[0].classList.add(`field-error`);
       passwordField[1].classList.add(`field-error`);
       confirmPasswordLabel.classList.add(`label-error`);
@@ -17,8 +17,7 @@ passwordField.forEach((input) => {
       passwordField[1].classList.remove(`field-match`);
       confirmPasswordLabel.classList.remove(`label-match`);
       passwordLabel.classList.remove(`label-match`);
-      mistMatchError.textContent="*Error! Passwords Must Match.";
-      formInputs.appendChild(mistMatchError);
+      passwordMatchErrorSpan.textContent="*Error! Passwords Must Match.";
     } else {
       passwordField[0].classList.remove(`field-error`);
       passwordField[1].classList.remove(`field-error`);
@@ -28,7 +27,7 @@ passwordField.forEach((input) => {
       passwordField[1].classList.add(`field-match`);
       confirmPasswordLabel.classList.add(`label-match`);
       passwordLabel.classList.add(`label-match`);
-      mistMatchError.textContent = ``;
+      passwordMatchErrorSpan.textContent = ``;
     }
   });
 });
